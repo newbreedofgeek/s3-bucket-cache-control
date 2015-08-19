46a7887afa77bac8b643a171a7e208a3a13d1cb1
Foxtel CT Team <ct.foxtel@gmail.com>
2015-08-19 12:50:37 +1000

    updated README, added an IF statement to do the update in batches

--- README.md
+++ README.md
@@ -9,4 +9,18 @@
 
 
 
-#### (Huge) Disclaimer: I built this as a trial and I have tested it… but be very careful using this script in a production environment. Do your due diligence first, there is a risk involved as we are updating the raw S3 objects on the fly!  
+
+## Get Stared...
+
+* Clone this repo and run "npm install" to install the dependencies
+* Export two ENV variables called "AWS_ACCESS_KEY_ID" and "AWS_SECRET_ACCESS_KEY" with your AWS Developer details as the value. This is a requirement of the "aws-sdk" NPM package. 
+* Once done, open app.js and add your bucket name to the "s3Bucket" variable
+* Set your desired Cache-Control header to the "cacheControl" variable
+* In Line 15, I have an IF statement that can be used to do this in smaller batches, I recommend this, but you can comment out the IF statement if you want.
+* Once done, run "node app.js"
+* Sit back until it finishes, then relaunch your website (then reload it again to enjoy the new cache control headers) and enjoy the speedy load time!!!
+
+
+
+
+#### (Huge) Disclaimer: I built this as a trial and I have tested it and used it on my own production blog called [wisdomtoinspire.com](http://wisdomtoinspire.com)… but be very careful using this script in a production environment. Do your due diligence first, there is a risk involved as we are updating the raw S3 objects on the fly!  
--- app.js
+++ app.js
@@ -9,38 +9,41 @@ S3.listObjects({Bucket: s3Bucket}, function(err, objData) {
         console.log(err, err.stack);
     }
     else {
-        console.log('.listObjects done, lets continue...');
+        console.log('.listObjects done, ' + objData.Contents.length + ' items found lets continue...');
 
-        objData.Contents.forEach(function(currObj) {
-            S3.headObject({Bucket: s3Bucket, Key: currObj.Key}, function(err, headData) {
-                if (err) {
-                    console.error('Cant complete .headObject, error details are: ');
-                    console.log(err, err.stack);
-                }
-                else {
-                    console.log('lets update ' + currObj.Key +'('+headData.ContentType+')' + ' to have the new cache control headers...');
+        objData.Contents.forEach(function(currObj, i) {
+            // Note: I do it in smaller batches of 100 as I had over 600 objects. I recommend this, but if you don't want to just comment out the IF statement below here
+            if (i > -1 && i < 100) {
+                S3.headObject({Bucket: s3Bucket, Key: currObj.Key}, function(err, headData) {
+                    if (err) {
+                        console.error('Cant complete .headObject, error details are: ');
+                        console.log(err, err.stack);
+                    }
+                    else {
+                        console.log('lets update ' + currObj.Key +'('+headData.ContentType+')' + ' to have the new cache control headers...');
 
-                    var paramObj = {
-                        Bucket: s3Bucket,
-                        Key: currObj.Key,
-                        ACL: 'public-read',
-                        CopySource : s3Bucket + '/' + currObj.Key,
-                        CacheControl: cacheControl,
-                        MetadataDirective: 'REPLACE',
-                        ContentType: headData.ContentType
-                    };
+                        var paramObj = {
+                            Bucket: s3Bucket,
+                            Key: currObj.Key,
+                            ACL: 'public-read',
+                            CopySource : s3Bucket + '/' + currObj.Key,
+                            CacheControl: cacheControl,
+                            MetadataDirective: 'REPLACE',
+                            ContentType: headData.ContentType
+                        };
 
-                    S3.copyObject(paramObj, function(err, objCpyData) {
-                        if (err) {
-                            console.error('Cant complete .copyObject for '+currObj.Key+', error details are: ');
-                            console.log(err, err.stack);
-                        }
-                        else {
-                            console.error('.copyObject done and headers should be updated for : '+currObj.Key);
-                        }
-                    });
-                }
-            });
+                        S3.copyObject(paramObj, function(err, objCpyData) {
+                            if (err) {
+                                console.error('Cant complete .copyObject for '+currObj.Key+', error details are: ');
+                                console.log(err, err.stack);
+                            }
+                            else {
+                                console.error('.copyObject done and headers should be updated for : '+currObj.Key);
+                            }
+                        });
+                    }
+                });
+            }
         });
     }
 });
